867badc6d49a59c842ed554565b54961e504e2ac
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-03-28 15:13:52 -0300

    Adicionado módulo Upload de Imagens

---
+++ 11-upload-imagens/src/config/db.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+    // user: 'Seu usuário do PostgreSQL aqui',
+    // password: 'Sua senha do PostgreSQL aqui',
+    host: 'localhost',
+    port: 5432,
+    database: 'launchstoredb'
+});
